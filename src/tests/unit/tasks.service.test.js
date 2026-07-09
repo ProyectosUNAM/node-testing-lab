2e155c992001026cf20445b58250dd636f3f1fa6
ProyectosUNAM <bricel.salas@gmail.com>
2026-07-09 14:52:47 -0500

    test: implementar Medición de Performance Nativa (Performance API)

--- src/tests/unit/tasks.service.test.js
+++ src/tests/unit/tasks.service.test.js
@@ -1,6 +1,21 @@
+const { performance } = require('perf_hooks'); // Importamos la API de Performance de Node.js
+
 const { createTask } = require("../../../src/services/tasks.service");
 
 describe("createTask (prueba unitaria)", () => {
+
+    // Medición de Performance Nativa (Performance API): 
+    let startTime; 
+    beforeAll(() => {
+        startTime = performance.now(); 
+    });
+    afterAll(() => {
+        const endTime = performance.now();
+        const duration = (endTime - startTime) / 1000; // Pasado a segundos
+        console.log(`\n⏱️  [MEDICIÓN PURA] Tiempo neto de los tests: ${duration.toFixed(4)} s\n`);
+    });
+
+
 
 //Ejercicio 1.1: Creación exitosa de una tarea
     test("crea la tarea cuando el titulo es valido", async () => {
